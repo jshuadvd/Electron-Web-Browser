eb0a52ac27ca02cd6ebf9e5f990903c752e52e83
jshuadvd <jshuadvd@icloud.com>
2015-12-13 23:57:01 -0800

    Electron Web Browser Application

---
+++ client/javascript/renderer.js
@@ -0,0 +1,25 @@
+var $ = require('jquery');
+
+$('#visit').click(function() {
+  var url =$('#url-input').val();
+  $('.tab.active').text(url);
+  $('.view.active').attr('src', url);
+});
+
+$('#new-tab').click(function() {
+  var url =$('#url-input').val();
+
+  $('.tab, .view').removeClass('active');
+
+  $('#tab-bar').append('<li class="tab active">' + url + '</li>');
+  $('#views').append('<webview class="view active" src="' + url +'"></webview>');
+});
+
+$('body').on('click','.tab', function() {
+  $('.tab, .view').removeClass('active');
+
+  var index = $(this).index();
+
+  $('.tab').eq(index).addClass('active');
+  $('.view').eq(index).addClass('active');
+});
